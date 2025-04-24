2aabf2e0916b46db16004496b070e1d1dac98a6c
akshbala2003 <abalakrishnan@presidio.com>
2025-04-24 12:19:13 +0530

    Initial commit

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Akshaya Balakrishnan on 23/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HelloWorld/HelloWorldTests/HelloWorldTests.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldTests.swift
+//  HelloWorldTests
+//
+//  Created by Akshaya Balakrishnan on 23/04/25.
+//
+
+import Testing
+@testable import HelloWorld
+
+struct HelloWorldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
